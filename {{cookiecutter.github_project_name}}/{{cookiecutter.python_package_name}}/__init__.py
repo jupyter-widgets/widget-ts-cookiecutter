cf9ae53800f9a2b82a392adbe3dd8d0f8f3a9dff
Vidar Tonaas Fauske <vidartf@gmail.com>
2017-09-18 11:15:29 +0200

    Fix version imports

--- {{cookiecutter.github_project_name}}/{{cookiecutter.python_package_name}}/__init__.py
+++ {{cookiecutter.github_project_name}}/{{cookiecutter.python_package_name}}/__init__.py
@@ -5,6 +5,6 @@
 # Distributed under the terms of the Modified BSD License.
 
 from .example import ExampleWidget
-from ._version import *
+from ._version import __version__, version_info
 
 from .nbextension import _jupyter_nbextension_paths
