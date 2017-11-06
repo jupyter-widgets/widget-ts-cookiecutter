9223b6eca316aff778dea08ddf0d8e3bc567892f
Steven Silvester <steven.silvester@ieee.org>
2017-11-06 05:05:39 -0600

    force a build

--- {{cookiecutter.github_project_name}}/release.js
+++ {{cookiecutter.github_project_name}}/release.js
@@ -5,6 +5,8 @@ var childProcess = require('child_process');
 var fs = require('fs-extra');
 var path = require('path');
 
+// Ensure we have built assets.
+childProcess.execSync('npm run build', { stdio: [0, 1, 2] });
 
 // Pack the lab extension to a tarball for release.
 var here = path.resolve('.');
@@ -15,7 +17,6 @@ var target = path.join(
 
 fs.ensureDir(target);
 
-// This also runs the build script because of the `prepack` script.
 childProcess.execSync('npm pack ' + here, {
   cwd: target,
   stdio: [0, 1, 2]
