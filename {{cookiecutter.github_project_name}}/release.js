41b75e10bef0c8c99c62f2bfa79cab07f1691d0d
Steven Silvester <steven.silvester@ieee.org>
2017-11-06 04:58:34 -0600

    Update release script

--- {{cookiecutter.github_project_name}}/release.js
+++ {{cookiecutter.github_project_name}}/release.js
@@ -5,6 +5,8 @@ var childProcess = require('child_process');
 var fs = require('fs-extra');
 var path = require('path');
 
+// Ensure we have built assets.
+childProcess.execSync('npm run build', { stdio: [0, 1, 2] });
 
 // Pack the lab extension to a tarball for release.
 var here = path.resolve('.');
