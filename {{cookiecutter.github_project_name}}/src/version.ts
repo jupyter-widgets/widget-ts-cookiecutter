edb930f0518c73314c0c00ae41d7423370240fbd
Jason Grout <jgrout6@bloomberg.net>
2018-12-22 21:25:03 -0800

    Add better docs for the module version.

--- {{cookiecutter.github_project_name}}/src/version.ts
+++ {{cookiecutter.github_project_name}}/src/version.ts
@@ -4,7 +4,10 @@
 const data = require('../package.json');
 
 /**
- * The current package version.
+ * The _model_module_version/_view_module_version this package implements.
+ *
+ * The html widget manager assumes that this is the same as the npm package
+ * version number.
  */
 export const MODULE_VERSION = data.version;
 
