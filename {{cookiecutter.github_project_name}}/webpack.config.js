09f65e4e55239e7204a2a5c6a310700ce07589e4
Jason Grout <jgrout6@bloomberg.net>
2018-12-22 21:21:05 -0800

    Add better comments for the webpack bundles.

--- {{cookiecutter.github_project_name}}/webpack.config.js
+++ {{cookiecutter.github_project_name}}/webpack.config.js
@@ -38,36 +38,17 @@ module.exports = [
     resolve,
   },
 
+  /**
+   * Embeddable {{ cookiecutter.npm_package_name }} bundle
+   *
+   * This bundle is almost identical to the notebook extension bundle. The only
+   * difference is in the configuration of the webpack public path for the
+   * static assets.
+   *
+   * The target bundle is always `dist/index.js`, which is the path required by
+   * the custom widget embedder.
+   */
   {
-    // embeddable bundle (e.g. for docs)
-    entry: './src/index.ts',
-    output: {
-      filename: 'embed-bundle.js',
-      path: path.resolve(__dirname, 'docs', 'source', '_static'),
-      library: "{{ cookiecutter.npm_package_name }}",
-      libraryTarget: 'amd'
-    },
-    module: {
-      rules: rules
-    },
-    devtool: 'source-map',
-    externals,
-    resolve,
-  },
-  {// Embeddable {{ cookiecutter.npm_package_name }} bundle
-    //
-    // This bundle is generally almost identical to the notebook bundle
-    // containing the custom widget views and models.
-    //
-    // The only difference is in the configuration of the webpack public path
-    // for the static assets.
-    //
-    // It will be automatically distributed by unpkg to work with the static
-    // widget embedder.
-    //
-    // The target bundle is always `dist/index.js`, which is the path required
-    // by the custom widget embedder.
-    //
     entry: './src/index.ts',
     output: {
         filename: 'index.js',
@@ -82,5 +63,28 @@ module.exports = [
     },
     externals,
     resolve,
+  },
+
+
+  /**
+   * Documentation widget bundle
+   *
+   * This bundle is used to embed widgets in the package documentation.
+   */
+  {
+    entry: './src/index.ts',
+    output: {
+      filename: 'embed-bundle.js',
+      path: path.resolve(__dirname, 'docs', 'source', '_static'),
+      library: "{{ cookiecutter.npm_package_name }}",
+      libraryTarget: 'amd'
+    },
+    module: {
+      rules: rules
+    },
+    devtool: 'source-map',
+    externals,
+    resolve,
   }
+
 ];
